639743efb430fffd6bf4e2e654d655b04a3a6420
Ibrahim ERTURK <ierturk@ieee.org>
2020-01-26 15:23:13 +0300

    Net --> UART over UDP is ok

--- main/AppMain.c
+++ main/AppMain.c
@@ -28,6 +28,12 @@
 
 #define PORT CONFIG_EXAMPLE_PORT
 
+#define ECHO_TEST_TXD  (GPIO_NUM_32)
+#define ECHO_TEST_RXD  (GPIO_NUM_16)
+#define ECHO_TEST_RTS  (UART_PIN_NO_CHANGE)
+#define ECHO_TEST_CTS  (UART_PIN_NO_CHANGE)
+#define BUF_SIZE (1024)
+
 static const char *TAG = "uart_gateway";
 
 static void udp_server_task(void *pvParameters)
@@ -36,6 +42,19 @@ static void udp_server_task(void *pvParameters)
     char addr_str[128];
     int addr_family;
     int ip_protocol;
+
+    uint8_t *data = (uint8_t *) malloc(BUF_SIZE);
+    uart_config_t uart_config = {
+        .baud_rate = 921600,
+        .data_bits = UART_DATA_8_BITS,
+        .parity    = UART_PARITY_DISABLE,
+        .stop_bits = UART_STOP_BITS_1,
+        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
+        .source_clk = UART_SCLK_APB,
+    };
+    uart_driver_install(UART_NUM_2, BUF_SIZE * 2, 0, 0, NULL, 0);
+    uart_param_config(UART_NUM_2, &uart_config);
+    uart_set_pin(UART_NUM_2, ECHO_TEST_TXD, ECHO_TEST_RXD, ECHO_TEST_RTS, ECHO_TEST_CTS);
 
     while (1) {
 
@@ -72,6 +91,10 @@ static void udp_server_task(void *pvParameters)
 
         while (1) {
 
+        	// UART Stuff
+        	// int len = uart_read_bytes(UART_NUM_2, data, BUF_SIZE, 20 / portTICK_RATE_MS);
+            // uart_write_bytes(UART_NUM_2, (const char *) data, len);
+
             ESP_LOGI(TAG, "Waiting for data");
             struct sockaddr_in6 source_addr; // Large enough for both IPv4 or IPv6
             socklen_t socklen = sizeof(source_addr);
@@ -95,6 +118,10 @@ static void udp_server_task(void *pvParameters)
                 ESP_LOGI(TAG, "Received %d bytes from %s:", len, addr_str);
                 ESP_LOGI(TAG, "%s", rx_buffer);
 
+                // Send to UART
+                uart_write_bytes(UART_NUM_2, (const char *) rx_buffer, len);
+
+                // Echo
                 int err = sendto(sock, rx_buffer, len, 0, (struct sockaddr *)&source_addr, sizeof(source_addr));
                 if (err < 0) {
                     ESP_LOGE(TAG, "Error occurred during sending: errno %d", errno);
@@ -112,52 +139,11 @@ static void udp_server_task(void *pvParameters)
     vTaskDelete(NULL);
 }
 
-#define ECHO_TEST_TXD  (GPIO_NUM_32)
-#define ECHO_TEST_RXD  (GPIO_NUM_16)
-#define ECHO_TEST_RTS  (UART_PIN_NO_CHANGE)
-#define ECHO_TEST_CTS  (UART_PIN_NO_CHANGE)
-
-#define BUF_SIZE (1024)
-
-static void echo_task(void *arg)
-{
-    /* Configure parameters of an UART driver,
-     * communication pins and install the driver */
-    uart_config_t uart_config = {
-        .baud_rate = 921600,
-        .data_bits = UART_DATA_8_BITS,
-        .parity    = UART_PARITY_DISABLE,
-        .stop_bits = UART_STOP_BITS_1,
-        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
-        .source_clk = UART_SCLK_APB,
-    };
-    uart_driver_install(UART_NUM_2, BUF_SIZE * 2, 0, 0, NULL, 0);
-    uart_param_config(UART_NUM_2, &uart_config);
-    uart_set_pin(UART_NUM_2, ECHO_TEST_TXD, ECHO_TEST_RXD, ECHO_TEST_RTS, ECHO_TEST_CTS);
-
-    // Configure a temporary buffer for the incoming data
-    uint8_t *data = (uint8_t *) malloc(BUF_SIZE);
-
-    while (1) {
-        // Read data from the UART
-        int len = uart_read_bytes(UART_NUM_2, data, BUF_SIZE, 20 / portTICK_RATE_MS);
-        // Write data back to the UART
-        uart_write_bytes(UART_NUM_2, (const char *) data, len);
-    }
-}
-
 void app_main(void)
 {
     ESP_ERROR_CHECK(nvs_flash_init());
     ESP_ERROR_CHECK(esp_netif_init());
     ESP_ERROR_CHECK(esp_event_loop_create_default());
-
-    /* This helper function configures Wi-Fi or Ethernet, as selected in menuconfig.
-     * Read "Establishing Wi-Fi or Ethernet Connection" section in
-     * examples/protocols/README.md for more information about this function.
-     */
     ESP_ERROR_CHECK(example_connect());
-
     xTaskCreate(udp_server_task, "udp_server", 4096, NULL, 5, NULL);
-    xTaskCreate(echo_task, "uart_echo_task", 1024, NULL, 10, NULL);
 }
